a7c81752cd5378d97778838dfa3cf6a07aea5bd9
cmesle <cecile.mesle@gmail.com>
2022-09-17 22:11:20 +0200

    suppr div wrap -> header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,13 @@ import Nav from "./Nav"
 
 function Header() {
     return (
-        <div>
-            <header>
-                <div className='top-header'>
-                    <img src={logo} className="header-logo" alt="logo" />
-                    <h1>Welcome to your social network</h1>
-                </div>
-                <Nav />
-            </header>
-        </div>
+        <header>
+            <div className='top-header'>
+                <img src={logo} className="header-logo" alt="logo" />
+                <h1>Welcome to your social network</h1>
+            </div>
+            <Nav />
+        </header>
     )
 }
 
