ca159e358840ce9359d056b847e7abd301d97650
cmesle <cecile.mesle@gmail.com>
2022-09-18 10:01:21 +0200

    ajout Outlet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
+import { Outlet } from 'react-router-dom';
 import logo from '../assets/logo.avif';
 import '../styles/header.css'
 import Nav from "./Nav"
 
 function Header() {
     return (
-        <header>
-            <div className='top-header'>
-                <img src={logo} className="header-logo" alt="logo" />
-                <h1>Welcome to your social network</h1>
-            </div>
-            <Nav />
-        </header>
+        <>
+            <header>
+                <div className='top-header'>
+                    <img src={logo} className="header-logo" alt="logo" />
+                    <h1>Welcome to your social network</h1>
+                </div>
+                <Nav />
+            </header>
+            <Outlet />
+        </>
     )
 }
 
