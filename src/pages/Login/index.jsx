ba07f05d9c8433f0407395ffd3253c1dd10bdbc4
cmesle <cecile.mesle@gmail.com>
2022-09-26 18:36:50 +0200

    cleaning

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Button from '../../components/Button';
 import UserForm from '../../components/UserForm';
 import '../../styles/main.css';
 
