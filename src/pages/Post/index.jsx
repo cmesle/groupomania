459290afe44dfd2c059da0dea149f6cd314a9e3f
cmesle <cecile.mesle@gmail.com>
2022-09-27 19:18:27 +0200

    Post Page (with data)

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,16 +1,66 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import axios from "axios"
+import styled from "styled-components"
+
+import colors from "../../utils/style/colors"
+
 import LikePost from "../../components/LikePost"
+import Button from "../../components/Button"
+import { PostToUpdateContext } from "../../utils/context"
+
+
+
+const StyledPost = styled.div`
+display: flex;
+flex-direction: column;
+width: 27%;
+padding: 10px;
+border: 1px solid ${colors.tertiairy};
+border-radius: 10px;
+`
+
 function Post(imageUrl, title, date, text) {
+
+    const postId = useParams()
+    const baseURL = `http://localhost:3001/api/post/${postId.id}`
+
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        axios.get(baseURL)
+            .then((res) => setPost(res.data))
+    }, [baseURL])
 
     return (
         <main>
-            <img src={imageUrl} alt='defaultPostImg' width='50%' />
-            <div>
-                <h2>{title}</h2>
-                <div>{date}</div>
-                <div>{text}</div>
-                <LikePost />
-            </div>
-        </main>
+
+            < StyledPost >
+                <div>author's name</div>
+                <img src={post.imageUrl} alt='defaultPostImg' width='50%' />
+                <div>
+                    <h2>{post.title}</h2>
+                    <div>{post.date}</div>
+                    <div>{post.text}</div>
+                    <LikePost />
+                    {/* {authorId===requestAnimationFrame.auth && */}
+                    <Link to=''>
+                        <PostToUpdateContext.Consumer>
+                            {(context) => (
+                                <Button name='modifier' onClick={(context) => {
+                                    context.setPostToUpdate(post)
+                                    console.log(context.postToUpdate)
+                                }} />
+                            )}
+                        </PostToUpdateContext.Consumer>
+                    </Link>
+                    <Button name='supprimer' />
+                    {/* } */}
+                </div>
+            </StyledPost>
+
+
+        </main >
     )
 }
 
