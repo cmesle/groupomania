d27599cbb7fbc5f53f6b38a47fae406231be0bb1
cmesle <cecile.mesle@gmail.com>
2022-09-29 18:32:05 +0200

    Header -> PostLayout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
@@ -7,11 +9,14 @@ import './index.css';
 import BeforeStarting from './components/BeforeStarting';
 import Login from './pages/Login';
 // import reportWebVitals from './reportWebVitals';
-import Header from './components/Header'
+import PostLayout from './pages/PostLayout';
 import Gallery from './pages/Gallery'
 import NewPost from './pages/NewPost';
+import Post from './pages/Post';
 import Signup from './pages/Signup';
 import NoPage from './pages/NoPage';
+import { PTUContextProvider } from './utils/context';
+
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -21,28 +26,31 @@ body {
   flex-grow: 1;
 }
 `
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      {/* <Header /> */}
-      <Routes>
-        <Route path="" element={<BeforeStarting />}>
-          <Route index element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-        <Route path="" element={<Header />}>
-          <Route path='gallery' element={<Gallery />} />
-          <Route path="newpost" element={<NewPost />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
+      <PTUContextProvider value={0}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="" element={<BeforeStarting />}>
+            <Route index element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+
+          <Route path="" element={<PostLayout />}>
+            <Route path='gallery' element={<Gallery />} />
+            <Route path="newpost" element={<NewPost />} />
+            <Route path="post/:id" element={<Post />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+
+        </Routes>
+      </PTUContextProvider>
     </BrowserRouter>
   </React.StrictMode >
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
