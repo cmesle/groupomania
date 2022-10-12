3b72490ae34642252db4dc6b78758e5678d0a658
cmesle <cecile.mesle@gmail.com>
2022-10-12 16:38:12 +0200

    getting user's pseudo

--- src/pages/PostLayout/index.jsx
+++ src/pages/PostLayout/index.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react'
 import { Outlet } from "react-router-dom";
+import axios from 'axios';
 import Header from "../../components/Header";
+import React from "react";
 
 function PostLayout() {
+    localStorage.setItem('PTU', '0')
+    const userId = localStorage.getItem('user')
+    /* getting the user's pseudo */
+    const [usersList, setUsersList] = useState()
+    const [userPseudo, setUserPseudo] = useState()
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/auth/user')
+            .then(res => {
+                setUserPseudo(res.data.filter(users => users._id === userId)[0].pseudo)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <>
-            <Header />
-            <Outlet />
+            <Header userPseudo={userPseudo} />
+            <Outlet
+            // usersList={usersList}
+            />
         </>
     )
 
