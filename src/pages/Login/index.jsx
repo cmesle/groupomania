3354b9eecdcf3393e70354e6ce5b224e8dbd30c0
cmesle <cecile.mesle@gmail.com>
2022-10-12 16:38:47 +0200

    change navigateTo

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,7 +6,7 @@ import UserForm from '../../components/UserForm';
 import '../../styles/main.css';
 
 const baseURL = "http://localhost:3001/api/auth/login"
-const navigate = ('/gallery')
+const navigateTo = ('gallery')
 const buttonName = "connexion"
 
 function Login() {
@@ -16,7 +16,7 @@ function Login() {
         Connexion
       </h1>
       <Link to='/signup'>pas encore inscrit ? Créer votre compte</Link>
-      <UserForm baseURL={baseURL} buttonName={buttonName} navigateTo={navigate} />
+      <UserForm baseURL={baseURL} buttonName={buttonName} navigateTo={navigateTo} />
     </main >
   )
 }
