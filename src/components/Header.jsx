47eed259437c0d34797e5ee305e3fa77e7f7d307
cmesle <cecile.mesle@gmail.com>
2022-10-13 15:54:36 +0200

    suppr getting user's pseudo (props instead)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import logo from '../assets/logo.avif';
 import '../styles/header.css'
 import Nav from "./Nav"
 
 
-function Header(props) {
-    /* getting the user's pseudo */
-    // const [usersList, setUsersList] = useState()
+function Header({ userPseudo }) {
 
-    // useEffect(() => {
-    //     // fetch('http://localhost:3001/api/auth/user')
-    //     //     .then(res => res.json())
-    //     //     .then(data => setUsersList(data))
-    //     //     .catch(err => console.log(err))
-
-    //     axios.get('http://localhost:3001/api/auth/user')
-    //         .then(res =>
-    //             setUsersList(res.data))
-    // }, [])
-
-    // console.log('layout', usersList)
     return (
         <>
             <header>
                 <div className='top-header'>
                     <img src={logo} className="header-logo" alt="logo" />
-                    <h1>Welcome to your social network, {props.userPseudo}</h1>
+                    <h1>Welcome to your social network, {userPseudo}</h1>
                 </div>
                 <Nav />
             </header>
