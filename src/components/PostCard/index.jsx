57eb2983f297c1e4ce99f736064c2fd4b31e65ef
cmesle <cecile.mesle@gmail.com>
2022-10-13 18:43:45 +0200

    refreshContext & post buttons

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,16 +1,52 @@
+import { useContext } from 'react'
 import { Link, useOutletContext } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
-import '../../styles/gallery.css'
-
-import colors from "../../utils/style/colors"
-
+import Button from "../Button"
 import LikePost from "../LikePost"
 
+import '../../styles/gallery.css'
+import { RefreshContext } from '../../utils/context'
 
-function PostCard({ /*id, imageUrl, title, date, text, author*/ post }) {
+function PostCard({ post/*, postsListRefresh*/ }) {
 
     const [usersList] = useOutletContext()
     const author = usersList.filter(user => (user._id === post.userId))[0].pseudo
+
+    /* -------------------------------------------------------------------------BOUTONS */
+    const baseURL = `http://localhost:3001/api/post/${post._id}`
+    // console.log(baseURL)
+    const token = localStorage.getItem('token')
+    const requestOptions = {
+        headers: { 'Authorization': 'Bearer ' + token }
+    }
+
+    const userId = localStorage.getItem('user')
+
+    const { incrementRefresh } = useContext(RefreshContext)
+    const navigate = useNavigate()
+    const navigateTo = '../gallery'
+    const deletePost = (e) => {
+        e.preventDefault()
+
+        axios.delete(baseURL, requestOptions)
+            .then(res => console.log(res))
+
+        incrementRefresh()
+        navigate(navigateTo)
+    }
+
+    const closePost = (e) => {
+        e.preventDefault()
+        navigate(navigateTo)
+    }
+
+    const modifyPost = (e) => {
+        e.preventDefault()
+        localStorage.setItem('PTU', post._id)
+        navigate('../updatepost')
+    }
 
     return (
         <div className='post-card'>
@@ -33,6 +69,20 @@ function PostCard({ /*id, imageUrl, title, date, text, author*/ post }) {
 
                 </div>
             </Link >
+            <div className='post-button'>
+                <Button type='button' name='fermer' action={closePost}
+                />
+                {userId === post.userId && (
+                    <div>
+                        <Button type='button' name='modifier' action={modifyPost} />
+                        <Button
+                            type='button'
+                            name='supprimer'
+                            action={deletePost} />
+
+                    </div>
+                )}
+            </div>
             <LikePost post={post} />
         </div >
     )
