c3c6ca912c6bb14f89bfde5b50309de20a48b7ce
cmesle <cecile.mesle@gmail.com>
2022-10-15 20:22:22 +0200

    icons and userRole

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,68 @@
-import { useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
+
 import { FilterContext } from '../utils/context'
 
 import '../styles/nav.css'
+import iconNew from '../assets/icon-nav/icon-new.avif'
+import iconMy from '../assets/icon-nav/icon-my.avif'
+import iconOut from '../assets/icon-nav/icon-out.avif'
+
+
+
 
 function Nav() {
 
+    /*---------------------------------------------------------------- GETTING USER'S ROLE */
+    const userId = localStorage.getItem('user')
+    const [userRole, setUserRole] = useState()
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/auth/user/${userId}`)
+            .then(res => { setUserRole(res.data.userRole) })
+    })
+
+    /*------------------------------------------------------------------- NAV LINKS LOGICS */
     const handleNewClick = () => { localStorage.setItem('PTU', '0') }
 
     const { toggleFilter, filter } = useContext(FilterContext)
     const handleFilterClick = () => { toggleFilter() }
 
-    const handleLogout = () => {
+    const handleLogoutClick = () => {
         localStorage.clear()
     }
 
     return (
         <nav>
             <ul>
+                {userRole !== 'admin' &&
+                    <>
+                        <li>
+                            <NavLink
+                                to='newpost'
+                                onClick={handleNewClick}>
+                                <img src={iconNew} alt="" />
+                                <span>nouvelle publication</span>
+                            </NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink
+                                to='gallery'
+                                onClick={handleFilterClick}>
+                                <img src={iconMy} alt="" />
+                                <span>
+                                    {filter === 'allPosts' ? 'mes publications' : 'toutes les publications'}
+                                </span>
+                            </NavLink>
+                        </li>
+                    </>
+                }
                 <li>
-                    <NavLink
-                        to='../newpost'
-                        onClick={handleNewClick}>
-                        nouvelle publication
+                    <NavLink to='/' onClick={handleLogoutClick}>
+                        <img src={iconOut} alt="" />
+                        <span>déconnexion</span>
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='../gallery'
-                        onClick={handleFilterClick}>
-                        {filter === 'allPosts' ? 'mes publications' : 'toutes les publications'}
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='../' onClick={handleLogout}>déconnexion</NavLink>
                 </li>
             </ul>
         </nav >
