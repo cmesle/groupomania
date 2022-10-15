ff0265a6604c2b293a21ad58a2d496fb5efa81f7
cmesle <cecile.mesle@gmail.com>
2022-10-15 20:21:02 +0200

    openPost & userRole

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Link, useOutletContext } from "react-router-dom"
+import { useOutletContext } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
 
@@ -8,15 +8,26 @@ import LikePost from "../LikePost"
 
 import '../../styles/gallery.css'
 import { RefreshContext } from '../../utils/context'
+import { useState } from 'react'
 
-function PostCard({ post/*, postsListRefresh*/ }) {
+function PostCard({ post }) {
 
+    const [openPost, setOpenPost] = useState('closed')
     const [usersList] = useOutletContext()
+    const userRole = useOutletContext()[2]
+
     const author = usersList.filter(user => (user._id === post.userId))[0].pseudo
+
+    const preDate = post.creationDate
+    const date = preDate.substring(0, 10)
+
+    const handleOpenPost = () => {
+        openPost === 'closed' ? setOpenPost('open') : setOpenPost('closed')
+    }
 
     /* -------------------------------------------------------------------------BOUTONS */
     const baseURL = `http://localhost:3001/api/post/${post._id}`
-    // console.log(baseURL)
+
     const token = localStorage.getItem('token')
     const requestOptions = {
         headers: { 'Authorization': 'Bearer ' + token }
@@ -24,23 +35,22 @@ function PostCard({ post/*, postsListRefresh*/ }) {
 
     const userId = localStorage.getItem('user')
 
-    const { incrementRefresh } = useContext(RefreshContext)
+    const { toggleRefresh } = useContext(RefreshContext)
     const navigate = useNavigate()
     const navigateTo = '../gallery'
-    const deletePost = (e) => {
-        e.preventDefault()
 
-        axios.delete(baseURL, requestOptions)
+    async function deletePost(e) {
+        e.preventDefault()
+        await axios.delete(baseURL, requestOptions)
             .then(res => console.log(res))
-
-        incrementRefresh()
+        await toggleRefresh()
         navigate(navigateTo)
     }
 
-    const closePost = (e) => {
-        e.preventDefault()
-        navigate(navigateTo)
-    }
+    // const closePost = (e) => {
+    //     e.preventDefault()
+    //     navigate(navigateTo)
+    // }
 
     const modifyPost = (e) => {
         e.preventDefault()
@@ -49,41 +59,50 @@ function PostCard({ post/*, postsListRefresh*/ }) {
     }
 
     return (
-        <div className='post-card'>
-            < Link to={`../post/${post._id}`} className='post-card--clickable'>
+        <div className={openPost === 'closed' ? 'post-card' : 'post-card--open'}>
+            <div className='post-card--clickable' onClick={handleOpenPost}>
 
                 {post.imageUrl &&
-                    <img src={post.imageUrl} alt='defaultPostImg' width='50%' />}
-                <div className="post-content">
+                    <img src={post.imageUrl} alt='defaultPostImg' width='100%' />}
+                <div className={openPost === 'closed' ? 'post-content' : 'post-content--open'}>
                     <div className="post-identity">
                         <div className="">
                             <p>{author}</p>
-                            <p className="post-date">{post.creationDate}</p>
+                            <p className="post-date">{date/*post.creationDate*/}</p>
                         </div>
                         <div>{post.title}</div>
+                        <div className='post-button'>
+                            {/* {openPost === 'open' &&
+                    <Button type='button' name='fermer' action={closePost}
+                    />
+                } */}
+
+                            {/* <div> */}
+                            {(userRole !== 'admin' && userId === post.userId) && (
+                                <Button type='button' name='modifier' action={modifyPost} />)}
+                            {(userRole === 'admin' || userId === post.userId) && (
+                                <Button
+                                    type='button'
+                                    name='supprimer'
+                                    action={deletePost} />
+                            )}
+                            {/* </div> */}
+                        </div>
                     </div>
 
                     {post.text &&
-                        <div className="post-text">{post.text}</div>
+                        <div className={openPost === 'closed' ? 'post-text' : 'post-text--open'}>
+                            {post.text}
+                        </div>
                     }
 
                 </div>
-            </Link >
-            <div className='post-button'>
-                <Button type='button' name='fermer' action={closePost}
-                />
-                {userId === post.userId && (
-                    <div>
-                        <Button type='button' name='modifier' action={modifyPost} />
-                        <Button
-                            type='button'
-                            name='supprimer'
-                            action={deletePost} />
-
-                    </div>
-                )}
             </div>
-            <LikePost post={post} />
+
+
+            {userRole !== 'admin' &&
+                <LikePost post={post} />
+            }
         </div >
     )
 }
