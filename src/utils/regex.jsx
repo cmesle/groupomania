c30e078f6d4740812f6254676ea43f422005fa60
cmesle <cecile.mesle@gmail.com>
2022-10-20 10:50:16 +0200

    regex in utils

---
+++ src/utils/regex.jsx
@@ -0,0 +1,6 @@
+export const pseudoRegex = /[A-Za-z0-9, \-, _]$/
+export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+export const passwordRegex = /^(?=.+[A-Za-z0-9])(?!.*[^A-Za-z0-9]).{8,}$/
+
+
+
