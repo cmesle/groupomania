d0ab01bd6b0b47ff8a81dfb0eb5b319f217daf8d
cmesle <cecile.mesle@gmail.com>
2022-10-20 11:56:08 +0200

    modif ternary for icon display

--- src/components/LikePost.jsx
+++ src/components/LikePost.jsx
@@ -34,41 +34,14 @@ function LikePost({ post }) {
                 setPostLikes(res.data.likes)
                 setAlreadyLiked(((res.data.usersLiked).findIndex(userId => userId === user)))
             })
-        console.log(alreadyLiked)
     }
 
-    // const ratePost = () => {
-    //     const requestData = {
-    //         'userId': user,
-    //         'like': ''
-    //     }
-
-    //     if (alreadyLiked === -1) {
-    //         requestData.like = "1"
-    //     } else {
-    //         requestData.like = "0"
-    //     }
-
-    //     axios.post(baseURL + `/like`, requestData, requestOptions)
-    //         .then(
-    //             axios.get(baseURL, requestOptions)
-    //                 .then(res => {
-    //                     setPostLikes(res.data.likes)
-    //                     console.log('posLikes', postLikes)
-    //                     setAlreadyLiked(((res.data.usersLiked).findIndex(userId => userId === user)))
-    //                     console.log('alreadyLiked', alreadyLiked)
-    //                 })
-    //         )
-
-    //     setRateTrigger(rateTrigger === 'off' ? 'on' : 'off')
-
-    // }
 
     return (
         <p className='post__rate'
             onClick={ratePost}
         >
-            {alreadyLiked ? <img src={thumbsUpOff} alt='' width='100%' /> : <img src={thumbsUpOn} alt='' width='100%' />}
+            {(alreadyLiked === -1) ? <img src={thumbsUpOff} alt='' width='100%' /> : <img src={thumbsUpOn} alt='' width='100%' />}
             <span>
                 {postLikes}
             </span></p >
