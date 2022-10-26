eba713b738d6d526e62f96830f902212cf6523dc
cmesle <cecile.mesle@gmail.com>
2022-10-26 16:44:46 +0200

    suppr GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 // import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
-import { FilterProvider, RefreshProvider } from './utils/context';
+import { FilterProvider, RefreshProvider } from './utils/context'
 
-import './index.css';
 import App from './App'
 
-
-const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-}
-#root {
-  flex-grow: 1;
-}
-`
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <RefreshProvider>
-      <FilterProvider>
-        <BrowserRouter>
-          < GlobalStyle />
-          <App />
-        </BrowserRouter>
-      </FilterProvider>
-    </RefreshProvider>
-  </React.StrictMode >
-);
-
+    <React.StrictMode>
+        <RefreshProvider>
+            <FilterProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </FilterProvider>
+        </RefreshProvider>
+    </React.StrictMode>
+)
