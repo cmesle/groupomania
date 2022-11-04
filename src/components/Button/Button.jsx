a6d6e03fc7b606dcdee72ecb2d86a5222520bda4
cmesle <cecile.mesle@gmail.com>
2022-11-04 09:55:30 +0100

    button styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,9 @@
 
 import './Button.css'
-// import styled from "styled-components"
 
-// const Button = styled.button`
-// color: green;
-// `
 function Button(props) {
     return (
-        <button type={props.type} disabled={props.disabled} onClick={props.action}>
+        <button type={props.type} /*disabled={props.disabled}*/ onClick={props.action}>
             <img src={props.icon} alt='' />
             <span>{props.name}</span>
         </button>
